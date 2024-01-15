e0e24408a7cc31eb122bd71f0870b234514d84e9
Tom Davies <tom@UKDSCs-Air.cust.communityfibre.co.uk>
2024-01-15 20:15:20 +0000

    add async call to run model in backend

--- services/client/src/index.js
+++ services/client/src/index.js
@@ -7,7 +7,11 @@ class App extends Component {
   constructor() {
     super();
     
+    this.state = {
+      image: {}
   }
+  }
+  
   componentDidMount() {
     this.getOutPut();
   }
@@ -15,24 +19,28 @@ class App extends Component {
   // new
   getOutPut() {
     axios.get(`${process.env.REACT_APP_API_SERVICE_URL}/ping`)
-    .then((res) => { console.log(res); })
-    .catch((err) => { console.log(err); });
+    .then((res) => { this.setState({ image: res.data }); }) // updated
+  .catch((err) => { console.log(err); });
   }
 
   render() {
+    const { image } = this.state;
+
     return (
       <section className="section">
         <div className="container">
           <div className="columns">
             <div className="column is-one-third">
               <br/>
-              <h1 className="title is-1">Snooker Tool</h1>
+              <h1 className="title is-1">Users</h1>
               <hr/><br/>
+              {/* Render the image message directly */}
+              <p className="box title is-4 username">{image.message}</p>
             </div>
           </div>
         </div>
       </section>
-    )
+    );
   }
 };
 
